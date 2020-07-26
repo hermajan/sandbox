1c15bed028b2ee4afdf9c9c429d56c7a7159d610
Jan Hermann <hermajan@gmail.com>
2020-07-26 03:59:24 +0200

    Added Nette 3

--- index.php
+++ index.php
@@ -1,12 +1,27 @@
 <?php
+declare(strict_types = 1);
+
+use App\Bootstrap;
 use Dotenv\Dotenv;
+use Contributte\Console\Application as ContributteApplication;
+use Nette\Application\Application as NetteApplication;
 
 require_once __DIR__."/vendor/autoload.php";
+require_once __DIR__."/app/Bootstrap.php";
 
-$dotenv = Dotenv::create(__DIR__);
+$dotenv = Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 $dotenv->required("APP_ENV")->notEmpty();
 
 // Add Nette
-require_once __DIR__."/app/bootstrap.php";
-
+if(php_sapi_name() === "cli") {
+    Bootstrap::boot()
+        ->createContainer()
+        ->getByType(ContributteApplication::class)
+        ->run();
+} else {
+    Bootstrap::boot()
+        ->createContainer()
+        ->getByType(NetteApplication::class)
+        ->run();
+}
